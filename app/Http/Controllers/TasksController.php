a8c65d44fa3003fdb5ef15b9b00ca8d0477295ff
Styx <Yusufibrahim117@hotmail.com>
2024-04-04 17:25:58 +0100

    add task listing and task creation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,65 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the Task.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('tasks/index');
+        $tasks = Task::all();
+        return view('tasks/index')->with(['tasks' => $tasks]);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created Task in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        Task::create(["name" => $request->name]);
+
+        return redirect()->back()->with('success','Task created successfully');
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Update the specified Task in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -72,7 +48,7 @@ class TasksController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified Task from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,18 +1,29 @@
 @extends('template')
 @section('head')
     <style>
-        body{background-color:#e6e6e6; }
+        body {
+            background-color: #e6e6e6;
+        }
     </style>
 @endsection
 
 @section('body')
-<div class="container">
-    <img src="{{ asset('images/logo.png') }}" alt="logo" class="mt-3">
+    <div class="container">
+        <img src="{{ asset('images/logo.png') }}" alt="logo" class="mt-3">
 
-    <div class="row mt-5">
+        <div class="row mt-5">
             <div class="col-md-4">
-                <input type="text" class="form-control" placeholder="Insert task name">
-                <button class="btn btn-primary w-100 mt-3">Add</button>
+                <form action="/tasks" method="POST">
+                    @csrf
+                    <input type="text" class="form-control" placeholder="Insert task name" name="name">
+                    <button class="btn btn-primary w-100 mt-3">Add</button>
+                    @if (session('success'))
+                        <p class="text-success text-center mt-2">{{ session('success') }}</p>
+                    @endif
+                    @foreach ($errors->all() as $error)
+                        <p class="text-danger text-center mt-2">{{ $error }}</p>
+                    @endforeach
+                </form>
             </div>
             <div class="col-md-8">
                 <div class="card">
@@ -24,14 +35,12 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Num</td>
-                                <td>Task Name</td>
-                            </tr>
-                            <tr>
-                                <td>Num</td>
-                                <td>Task Name</td>
-                            </tr>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td>{{$task->id}}</td>
+                                    <td>{{$task->name}}</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
